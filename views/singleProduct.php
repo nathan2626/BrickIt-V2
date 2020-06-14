47506d25399e4fe4572922b337c8976f984c1f35
nathan2626 <61419599+nathan2626@users.noreply.github.com>
2020-06-14 23:41:28 +0200

    Bonus : User comment completed, will need to review the mvc part, security and style.

--- views/singleProduct.php
+++ views/singleProduct.php
@@ -37,6 +37,73 @@
                 </div>
             </div>
         </div>
+        <?php
+        if (isset($_GET['id']) &&  !empty($_GET['id'])) {
+            $db = dbConnect();
+//            && isset($_SESSION['user']) && $_SESSION['user']['is_admin'] == 0
+            $productId = htmlspecialchars($_GET['id']);
+            $username = htmlspecialchars($_SESSION['user']['first_name']);
+
+
+            if (isset($_POST['submit_comment']) ) {
+                if ($_SESSION['user']['first_name'] == $username) {
+                    if (isset($_POST['pseudo'], $_POST['comment']) && !empty($_POST['pseudo']) && !empty($_POST['comment'])) {
+                        $pseudo = htmlspecialchars($_POST['pseudo']);
+                        $comment = htmlspecialchars($_POST['comment']);
+                        $notation = htmlspecialchars($_POST['notation']);
+
+                        if (strlen($pseudo) < 20 && $notation >= 0 && $notation <= 5) {
+                            $query = $db->prepare('INSERT INTO comments (comment, pseudo, product_id, username, notation) VALUES (?, ?, ?, ?, ?)');
+                            $query->execute([$comment, $pseudo, $productId, $username, $notation]);
+                            $msg = "Votre commentaire a bien été posté !";
+                        } else {
+                            $msg = "Le pseudo peut contenir seulement 20 caractères et la note entre 0 et 5 inclus.";
+                        }
+
+                    } else {
+                        $msg = "Tous les champs doivent être complétés";
+                    }
+                } else {
+                    $msg = "Vous avez déjà posté un commentaire pour ce produit";
+
+                }
+            }
+            $allComments = $db->prepare('SELECT * FROM comments WHERE product_id = ? ORDER BY id DESC');
+            $allComments->execute([$productId]);
+        }
+        ?>
+        <?php if ($_SESSION['user']['first_name'] == $username): ?>
+        <div class="allCommentsPart">
+            <div>
+                <h1>Postez votre commentaire !</h1>
+                <form method="post">
+                    <input type="text" name="pseudo" placeholder="Votre pseudo"> <br>
+                    <input type="number" name="notation" placeholder="Note entre 0 et 5 inclus"> <br>
+                    <textarea name="comment" placeholder="Votre commentaire..."></textarea><br>
+                    <input type="submit" value="Poster" name="submit_comment">
+                </form>
+            </div>
+        <?php endif; ?>
+        <?php if (isset($msg)): ?>
+            <div>
+                <p><?= $msg ?></p>
+            </div>
+        <?php endif; ?>
+            <details>
+                <summary>Commentaires :</summary>
+                <div>
+                    <?php foreach ($allComments as $comments): ?>
+                        <div>
+                            <p><?= $comments['username'] ?> sous le nom de <?= $comments['pseudo'] ?> a posté ce commentaire et cette note :</p><br>
+                            <p><?= $comments['comment'] ?></p><br>
+                            <p>Et une note de <?= $comments['notation'] ?></p><br><br><br>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </details>
+        </div>
+
+
     </article>
     <article class="productsCategory">
         <h1 class="titleRandom">Produits qui peut vous interesser</h1>
@@ -74,43 +141,6 @@
                 </a>
             </div>
             <?php endfor; ?>
-
-            <!--            <div class="sub-item introImage">-->
-<!--                <a href="">-->
-<!--                    <img src="./assets/images/imageMarvelCategories.jpg" alt="">-->
-<!--                    <div class="overlay">-->
-<!--                        <h1>Star Wars</h1>-->
-<!--                        <div class="separator separatorProductsCategory"></div>-->
-<!--                        <div class="price">-->
-<!--                            <h2>19.99$ <a href=""><i class="fas fa-shopping-bag"></i></a></h2>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </a>-->
-<!--            </div>-->
-<!--            <div class="sub-item introImage">-->
-<!--                <a href="">-->
-<!--                    <img src="./assets/images/imageMarvelCategories.jpg" alt="">-->
-<!--                    <div class="overlay">-->
-<!--                        <h1>Star Wars</h1>-->
-<!--                        <div class="separator separatorProductsCategory"></div>-->
-<!--                        <div class="price">-->
-<!--                            <h2>19.99$ <a href=""><i class="fas fa-shopping-bag"></i></a></h2>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </a>-->
-<!--            </div>-->
-<!--            <div class="sub-item introImage">-->
-<!--                <a href="">-->
-<!--                    <img src="./assets/images/imageMarvelCategories.jpg" alt="">-->
-<!--                    <div class="overlay">-->
-<!--                        <h1>Star Wars</h1>-->
-<!--                        <div class="separator separatorProductsCategory"></div>-->
-<!--                        <div class="price">-->
-<!--                            <h2>19.99$ <a href=""><i class="fas fa-shopping-bag"></i></a></h2>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </a>-->
-<!--            </div>-->
         </div>
     </article>
 </main>
