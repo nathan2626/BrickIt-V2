8916a490082d19fc28f55ac3b3701cc027507f78
nathan2626 <61419599+nathan2626@users.noreply.github.com>
2020-06-19 17:19:45 +0200

    Readme

--- admin/views/partials/header.php
+++ admin/views/partials/header.php
@@ -6,23 +6,7 @@
     <div class="overlayNav"></div>
     <div class="containerNav">
         <nav class="colorCategoryProducts">
-            <h1 class="brickIt"><a href="../../views/index.php">Brick'It</a></h1>
-            <ul>
-                <li><a href="../../views/index.php">Accueil</a></li>
-                <li><a href="../../views/categories.php">Catégories</a>
-                    <ul class="submenu">
-                        <li><a href="">Harry Potter</a></li>
-                        <li><a href="">Disney</a></li>
-                        <li><a href="">Star Wars</a></li>
-                        <li><a href="">Warcraft</a></li>
-                    </ul>
-                </li>
-                <li><a href="../../views/game.html" class="exception">Jeu</a></li>
-                <li><a href="../../views/account.php">Compte</a></li>
-                <li><a href="../../views/contact.php">Contact</a></li>
-                <li><a href=""><i class="fas fa-search search"></i></a></li>
-                <li><a href=""><i class="fas fa-shopping-bag bag"></i></a></li>
-            </ul>
+            <h1 class="brickIt"><a href="../index.php">Brick'It</a></h1>
         </nav>
     </div>
 </header> <!-- Header -->
